b40899c33a8006afedc0a170c68a5e7b6cddb0a2
alex <alex>
2017-06-17 18:06:21 +0200

    edit file

--- utils/footbet.py
+++ utils/footbet.py
@@ -1,5 +1,5 @@
 import dataiku as dk
-from utils.sql_calls import footbet_lstm_simple,footbet_lstm_simple2,footbet_lstm_advanced
+from utils.sql_calls import footbet_lstm_simple,footbet_lstm_simple2,footbet_lstm_advanced,footbet_lstm_elo
 
 def get_target(home_goal,away_goal):
     if home_goal> away_goal:
@@ -14,7 +14,12 @@ def get_target_bin(home_goal,away_goal):
         return [1,0]
     else:
         return [0,1]
-      
+
+def get_club_histo_simple(club_id,match_dt,data):
+    executor = dk.core.sql.SQLExecutor2(dataset=data)
+    mess = footbet_lstm_elo(club_id,match_dt)
+    return executor.query_to_df(mess)
+
 def get_club_histo_simple(club_id,match_dt,data):
     executor = dk.core.sql.SQLExecutor2(dataset=data)
     mess = footbet_lstm_simple(club_id,match_dt)
