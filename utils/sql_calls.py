770ee5af099926ef94fac97bb60454eac9061263
alex <alex>
2017-06-17 10:32:25 +0200

    edit file

---
+++ utils/sql_calls.py
@@ -0,0 +1,39 @@
+def footbet_lstm_advanced(club_id,match_dt):
+    return """ select home_away_flag,club_point /*,home_rank,away_rank,home_goal,away_goal*/
+    from
+    (select
+           case when club_id = home_id then 1 else 0 end as home_away_flag
+          ,home_rank,away_rank
+          ,case when (club_id = home_id and home_goal > away_goal) or (club_id = away_id and home_goal < away_goal) then 3
+                when home_goal = away_goal then 1
+                else 0
+            end as club_point
+          ,home_goal,away_goal
+          ,match_dt
+          ,row_number() over(order by match_dt desc) as rk 
+
+    from "DATAIMPORT_foot_games_p"
+
+    where club_id = '{0}' and match_dt < '{1}') tmp
+    where rk <=10
+     """.format(club_id,match_dt) 
+
+def footbet_lstm_simple(club_id,match_dt):
+    return """select home_win,home_draw,home_defeat,away_win,away_draw,away_defeat
+from
+(select
+       case when club_id = home_id and home_goal > away_goal then 1 else 0 end as home_win
+      ,case when club_id = home_id and home_goal = away_goal then 1 else 0 end as home_draw
+      ,case when club_id = home_id and home_goal < away_goal then 1 else 0 end as home_defeat
+ 
+      ,case when club_id = away_id and home_goal < away_goal then 1 else 0 end as away_win
+      ,case when club_id = away_id and home_goal = away_goal then 1 else 0 end as away_draw
+      ,case when club_id = away_id and home_goal > away_goal then 1 else 0 end as away_defeat
+
+      ,row_number() over(order by match_dt desc) as rk 
+    
+from "DATAIMPORT_foot_games_p"
+
+where club_id = '{0}' and match_dt < '{1}}') tmp
+where rk <=10
+""".format(club_id,match_dt)
