7e114cfcffafc1229287c4ee496f1bebb9083503
alex <alex>
2017-06-18 00:47:30 +0200

    edit file

--- utils/footbet.py
+++ utils/footbet.py
@@ -1,5 +1,5 @@
 import dataiku as dk
-from utils.sql_calls import footbet_lstm_elo_simple_global,
+from utils.sql_calls import footbet_lstm_elo_global,footbet_lstm_elo_flag,footbet_lstm_simple2
 
 def get_target(home_goal,away_goal):
     if home_goal> away_goal:
@@ -15,18 +15,21 @@ def get_target_bin(home_goal,away_goal):
     else:
         return 0
 
-def get_club_histo_elo(club_id,data):
+def get_club_histo_elo_global(club_id,data):
     executor = dk.core.sql.SQLExecutor2(dataset=data)
-    mess = footbet_lstm_elo_simple(club_id)
+    mess = footbet_lstm_elo_global(club_id)
     return executor.query_to_df(mess)
+
+def get_club_histo_elo_flag(home_flag,club_id,data):
+    executor = dk.core.sql.SQLExecutor2(dataset=data)
+    mess = footbet_lstm_elo_simple(home_flag,club_id)
+    return executor.query_to_df(mess)
+
+
+#########################
 
 def get_club_histo_simple2(club_id,match_dt,data):
     executor = dk.core.sql.SQLExecutor2(dataset=data)
     mess = footbet_lstm_simple2(club_id,match_dt)
-    return executor.query_to_df(mess)
-
-def get_club_histo_elo_home(home_flag,club_id,match_dt,w,data):
-    executor = dk.core.sql.SQLExecutor2(dataset=data)
-    mess = footbet_lstm_elo_home(home_flag,club_id,match_dt,w)
     return executor.query_to_df(mess)
 
