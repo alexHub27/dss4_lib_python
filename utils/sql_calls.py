306edf105b37aa82b5e8c8b53b726c80146ba025
alex <alex>
2017-06-17 14:34:33 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -1,6 +1,7 @@
 import dataiku as dk 
 
 def footbet_lstm_advanced(club_id,match_dt):
+    """ Return a (10,6) Matrix"""
     return """ select home_away_flag,club_point,home_rank,away_rank,home_goal,away_goal
     from
     (select
@@ -21,10 +22,10 @@ def footbet_lstm_advanced(club_id,match_dt):
      """.format(club_id,match_dt) 
 
 def footbet_lstm_simple(club_id,match_dt):
+    """ Return a (10,6) Matrix"""
     return """select home_win,home_draw,home_defeat,away_win,away_draw,away_defeat 
 from
 (select
-    
       case when club_id = home_id and home_goal > away_goal then 1 else 0 end as home_win
       ,case when club_id = home_id and home_goal = away_goal then 1 else 0 end as home_draw
       ,case when club_id = home_id and home_goal < away_goal then 1 else 0 end as home_defeat
@@ -42,6 +43,7 @@ where rk <=10
 """.format(club_id,match_dt)
 
 def footbet_lstm_simple2(club_id,match_dt):
+    """ Return a (10,6) Matrix"""
     return """select home_win,home_draw,home_defeat,away_win,away_draw,away_defeat
 from
 (select club_id,match_dt,home_goal,away_goal
