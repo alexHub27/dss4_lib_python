458a61f527b4ecf4e47d8216eaa8b8964b250075
alex <alex>
2017-06-17 13:18:22 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -40,3 +40,25 @@ where club_id = '{0}' and match_dt < '{1}') tmp
 where rk <=10
 """.format(club_id,match_dt)
 
+def footbet_lstm_simple2(club_id,match_dt):
+    return """select adv_rank,home_win,home_draw,home_defeat,away_win,away_draw,away_defeat
+from
+(select
+       case when club_id = home_id then away_rank else home_rank as adv_rank
+      ,case when club_id = home_id and home_goal > away_goal then home_goal - away_goal else 0 end as home_win
+      ,case when club_id = home_id and home_goal = away_goal then 1 else 0 end as home_draw
+      ,case when club_id = home_id and home_goal < away_goal then home_goal - away_goal else 0 end as home_defeat
+ 
+      ,case when club_id = away_id and home_goal < away_goal then home_goal - away_goal else 0 end as away_win
+      ,case when club_id = away_id and home_goal = away_goal then 1 else 0 end as away_draw
+      ,case when club_id = away_id and home_goal > away_goal then home_goal - away_goal else 0 end as away_defeat
+
+      ,row_number() over(order by match_dt desc) as rk 
+    
+from "DATAIMPORT_foot_games_p"
+
+where club_id = '{0}' and match_dt < '{1}') tmp
+where rk <=10
+""".format(club_id,match_dt)
+
+
