0a98d32d9c5be22c40a32dd3335059272cb76273
alex <alex>
2017-06-18 12:32:50 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -31,7 +31,23 @@ from
       ,row_number() over(order by match_dt desc) as rk
       ,match_dt
 from "FOOTBET_{4}"
-where {0} = '{1}' and match_dt >= '{2}') tmp
+where {0} = '{1}' and match_dt <= '{2}') tmp
+where rk <= {3}
+order by match_dt
+""".format(home_flag,club_id,match_dt,w,dataNm)
+
+def footbet_lstm_elo_form_global(club_id,match_dt,w,dataNm='elo_rank_club_test'):
+    """ Supposed to be used as a batch generator for Model pre trained. Those models will then give features we will use in another
+        concatenated Dense model."""
+    return """ select proba_club,case when rk = 1 then 0 else cast(point_club as numeric) end as point_club
+from
+(select 
+       proba_home as proba_club
+      ,point_home as point_club
+      ,row_number() over(order by match_dt desc) as rk
+      ,match_dt
+from "FOOTBET_{4}"
+where (home_id = '{0}' or away_id = '{0}') and match_dt <= '{2}') tmp
 where rk <= {3}
 order by match_dt
 """.format(home_flag,club_id,match_dt,w,dataNm)
