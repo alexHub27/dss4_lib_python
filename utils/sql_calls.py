67125cfe8e48660bb7235924cb2b253a7b5d198a
alex <alex>
2017-06-18 18:09:28 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -5,10 +5,9 @@ def footbet_lstm_elo_global(club_id,dataNm):
     return """select case when home_id = '{0}' then 1 else 0 end as home_flag
       ,case when home_id = '{0}' then proba_home else proba_away end as proba_club
       ,case when home_id = '{0}' then point_home else point_away end as point_club
-      ,case when (home_id = '{0}' and home_goal > away_goal) or (away_id = '{0}' and home_goal < away_goal) 
-            then 1 
-            else 0
-        end as target
+      ,case when home_goal > away_goal then 1 else 0 end as target
+
+
 from "FOOTBET_{1}"
 where home_id = '{0}' or away_id = '{0}'
 order by match_dt""".format(club_id,dataNm)
