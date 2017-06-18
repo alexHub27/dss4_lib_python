4969c724edd2864b1ff606e059e24ac85c1d014f
alex <alex>
2017-06-18 19:29:04 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -1,23 +1,6 @@
 import dataiku as dk 
 
-def footbet_lstm_simple(club_id,dataNm):
-    """ Return a (10,6) Matrix"""
-    return """select home_win,home_draw,home_defeat,away_win,away_draw,away_defeat,target
-from
-(select
-      case when club_id = home_id and home_goal > away_goal then 1 else 0 end as home_win
-      ,case when club_id = home_id and home_goal = away_goal then 1 else 0 end as home_draw
-      ,case when club_id = home_id and home_goal < away_goal then 1 else 0 end as home_defeat
- 
-      ,case when club_id = away_id and home_goal < away_goal then 1 else 0 end as away_win
-      ,case when club_id = away_id and home_goal = away_goal then 1 else 0 end as away_draw
-      ,case when club_id = away_id and home_goal > away_goal then 1 else 0 end as away_defeat
-      
-      ,case when home_goal>away_goal then 1 else 0 end as target,match_dt
-from "FOOTBET_{1}"
-where club_id = '{0}') tmp
-order by match_dt
-""".format(club_id,match_dt)
+
 
 def footbet_lstm_elo_global(club_id,dataNm):
     return """select case when home_id = '{0}' then 1 else 0 end as home_flag
@@ -29,7 +12,8 @@ def footbet_lstm_elo_global(club_id,dataNm):
             end as rank_adv */
       ,case when home_id = '{0}' then proba_home else proba_away end as proba_club
       ,case when home_id = '{0}' then point_home else point_away end as point_club
-      ,case when home_goal > away_goal then 1 else 0 end as target
+      ,case when (home_id = '{0}' and home_goal>away_goal) or (away_id = '{0}' and home_goal<=away_goal)
+            then 1 else 0 end as target
 
 from "FOOTBET_{1}"
 where home_id = '{0}' or away_id = '{0}'
