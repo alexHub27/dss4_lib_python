0d11572d87c85417a9623ec27e797067cf8de7ea
alex <alex>
2017-06-18 18:40:22 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -3,8 +3,12 @@ import dataiku as dk
 
 def footbet_lstm_elo_global(club_id,dataNm):
     return """select case when home_id = '{0}' then 1 else 0 end as home_flag
-      ,case when home_id = '{0}' then home_rank else away_rank end as rank_club
-      ,case when home_id = '{0}' then away_rank else home_rank end as rank_adv
+      ,case when home_id = '{0}' then (cast(home_rank as numeric) -1000)/60 
+            else (cast(away_rank as numeric) -1000)/60
+            end as rank_club
+      ,case when home_id = '{0}' then (cast(away_rank as numeric) -1000)/60 
+            else (cast(home_rank as numeric) -1000)/60
+            end as rank_adv
       ,case when home_id = '{0}' then proba_home else proba_away end as proba_club
       ,case when home_id = '{0}' then point_home else point_away end as point_club
       ,case when home_goal > away_goal then 1 else 0 end as target
