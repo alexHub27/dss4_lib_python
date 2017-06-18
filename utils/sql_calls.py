6b32af35cee1e5a8ed87f9315bb462f3bd57a527
alex <alex>
2017-06-18 16:33:53 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -13,6 +13,22 @@ from "FOOTBET_{1}"
 where home_id = '{0}' or away_id = '{0}'
 order by match_dt""".format(club_id,dataNm)
 
+def footbet_lstm_elo_form_global(club_id,match_dt,w,dataNm='elo_rank_club_test'):
+    """ Supposed to be used as a batch generator for Model pre trained. Those models will then give features we will use in another
+        concatenated Dense model."""
+    return """ select home_flag,proba_club,case when rk = 1 then 0 else cast(point_club as numeric) end as point_club
+from
+(select case when home_id = '{0}' then 1 else 0 end as home_flag
+      ,proba_home as proba_club
+      ,point_home as point_club
+      ,row_number() over(order by match_dt desc) as rk
+      ,match_dt
+from "FOOTBET_{3}"
+where (home_id = '{0}' or away_id = '{0}') and match_dt <= '{1}') tmp
+where rk <= {2}
+order by match_dt
+""".format(club_id,match_dt,w,dataNm)
+
 def footbet_lstm_elo_flag(home_flag,club_id,dataNm):
     return """ select
         proba_home,point_home,case when home_goal > away_goal then 1 else 0 end as target
@@ -35,22 +51,6 @@ where {0} = '{1}' and match_dt <= '{2}') tmp
 where rk <= {3}
 order by match_dt
 """.format(home_flag,club_id,match_dt,w,dataNm)
-
-def footbet_lstm_elo_form_global(club_id,match_dt,w,dataNm='elo_rank_club_test'):
-    """ Supposed to be used as a batch generator for Model pre trained. Those models will then give features we will use in another
-        concatenated Dense model."""
-    return """ select home_flag,proba_club,case when rk = 1 then 0 else cast(point_club as numeric) end as point_club
-from
-(select case when home_id = '{0}' then 1 else 0 end as home_flag
-      ,proba_home as proba_club
-      ,point_home as point_club
-      ,row_number() over(order by match_dt desc) as rk
-      ,match_dt
-from "FOOTBET_{3}"
-where (home_id = '{0}' or away_id = '{0}') and match_dt <= '{1}') tmp
-where rk <= {2}
-order by match_dt
-""".format(club_id,match_dt,w,dataNm)
 
 ########################
 
