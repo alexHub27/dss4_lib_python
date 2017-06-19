4fa29739075b2eda17d03d3b9e930d23792158a6
alex <alex>
2017-06-20 00:34:06 +0200

    edit file

--- utils/sql_calls.py
+++ utils/sql_calls.py
@@ -35,31 +35,48 @@ order by l.match_dt""".format(club_id,dataNm)
 
 def footbet_lstm_elo_global(club_id,dataNm):
     return """select
-       case when home_id = '{0}' 
+       case when l.home_id = '{0}' 
             then 1
             else 0 
             end as flag_home 
-      ,case when home_id = '{0}' 
-            then proba_home 
-            else proba_away 
+      ,case when l.home_id = '{0}'
+            then (cast(l.home_rank as numeric) -1000)/60
+            else (cast(l.away_rank as numeric) -1000)/60
+            end as rank_club
+      ,case when l.home_id = '{0}'
+            then (cast(l.away_rank as numeric) -1000)/60
+            else (cast(l.home_rank as numeric) -1000)/60
+            end as rank_adv
+      ,case when l.home_id = '{0}' 
+            then l.proba_home 
+            else l.proba_away 
             end as proba_club
-      ,case when home_id = '{0}' 
-            then point_home 
-            else point_away 
+      ,case when l.home_id = '{0}' 
+            then l.point_home 
+            else l.point_away 
             end as point_club
-      ,case when (home_id = '{0}' and home_goal>away_goal) or (away_id = '{0}' and home_goal<=away_goal)
-            then 1 else 0 end as target
 
-from "FOOTBET_{1}"
+      ,case when (l.home_id = '{0}' and l.home_goal>l.away_goal) or (l.away_id = '{0}' and l.home_goal<=l.away_goal) then 1
+            else 0 end as target
+
+from "FOOTBET_{1}" l
 where home_id = '{0}' or away_id = '{0}'
 order by match_dt""".format(club_id,dataNm)
 
 def footbet_lstm_elo_form_global(club_id,match_dt,w,dataNm='elo_rank_club_test'):
     """ Supposed to be used as a batch generator for Model pre trained. Those models will then give features we will use in another
         concatenated Dense model."""
-    return """ select home_flag,proba_club,case when rk = 1 then 0 else cast(point_club as numeric) end as point_club
+    return """ select home_flag,rank_club,rank_adv,proba_club,case when rk = 1 then 0 else cast(point_club as numeric) end as point_club
 from
 (select case when home_id = '{0}' then 1 else 0 end as home_flag
+      ,case when home_id = '{0}'
+            then (cast(home_rank as numeric) -1000)/60
+            else (cast(away_rank as numeric) -1000)/60
+            end as rank_club
+      ,case when home_id = '{0}'
+            then (cast(away_rank as numeric) -1000)/60
+            else (cast(home_rank as numeric) -1000)/60
+            end as rank_adv
       ,case when home_id = '{0}' then proba_home else proba_away end as proba_club
       ,case when home_id = '{0}' then point_home else point_away end as point_club
       ,row_number() over(order by match_dt desc) as rk
@@ -71,8 +88,14 @@ order by match_dt
 """.format(club_id,match_dt,w,dataNm)
 
 def footbet_lstm_elo_flag(home_flag,club_id,dataNm):
-    return """ select proba_home,point_home
-        
+    return """ select 
+         case when '{1}'='home_id' then (cast(home_rank as numeric)-1000)/60
+              else (cast(away_rank as numeric)-1000)/60
+              end as rank_club
+        ,case when '{1}'='home_id' then (cast(away_rank as numeric)-1000)/60
+              else (cast(home_rank as numeric)-1000)/60
+              end as rank_adv
+        ,proba_home,point_home
         ,case when ('{1}'='home_id' and home_goal > away_goal) or ('{1}'='away_id' and home_goal <= away_goal) 
               then 1 
               else 0 
@@ -84,10 +107,16 @@ def footbet_lstm_elo_flag(home_flag,club_id,dataNm):
 def footbet_lstm_elo_form_flag(home_flag,club_id,match_dt,w,dataNm='elo_rank_club_test'):
     """ Supposed to be used as a batch generator for Model pre trained. Those models will then give features we will use in another
         concatenated Dense model."""
-    return """ select proba_club,case when rk = 1 then 0 else cast(point_club as numeric) end as point_club
+    return """ select rank_club,rank_adv,proba_club,case when rk = 1 then 0 else cast(point_club as numeric) end as point_club
 from
 (select 
-       proba_home as proba_club
+       case when '{1}'='home_id' then (cast(home_rank as numeric)-1000)/60
+            else (cast(away_rank as numeric)-1000)/60
+            end as rank_club
+      ,case when '{1}'='home_id' then (cast(away_rank as numeric)-1000)/60
+            else (cast(home_rank as numeric)-1000)/60
+            end as rank_adv
+      ,proba_home as proba_club
       ,point_home as point_club
       ,row_number() over(order by match_dt desc) as rk
       ,match_dt
