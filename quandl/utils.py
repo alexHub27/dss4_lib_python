9981770fbda5160d3904204cc65283f1e5695f54
alex <alex>
2017-12-03 14:45:58 +0100

    edit file

--- quandl/utils.py
+++ quandl/utils.py
@@ -5,3 +5,13 @@ def get_tickerDic(dfNm="sp500_ticker"):
     df_sym= dk.Dataset(dfNm).get_dataframe()
     sectors = np.unique(df_sym["GICS Sector"].values)
     return {tck:df_sym["GICS Sector"].values[idx] for idx,tck in enumerate(df_sym["Ticker symbol"].values)} 
+
+def get_price_dataframe(dfNm="wiki_sp500_daily_2007"):
+    dfs = dk.Dataset(dfNm).get_dataframe()
+    dfs["Date"] = pd.to_datetime(dfs.Date.values)
+    dfs.index = dfs.Date
+    return dfs 
+
+def get_Z(ticker_X,ticker_Y,beta,const,timeStamp,df_data):
+    df_data = df_data.loc[df_data.index<timeStamp][[ticker_X,ticker_Y]]
+    return 
