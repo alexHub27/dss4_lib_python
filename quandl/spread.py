352aae7604408e1e80442020394f8b0dac75970a
alex <alex>
2017-12-03 15:33:16 +0100

    edit file

---
+++ quandl/spread.py
@@ -0,0 +1,20 @@
+def get_Z(ticker_X,ticker_Y,beta,timeStamp,df_data):
+    start = timeStamp - dt.timedelta(days=252*7.5)
+    df_data = df_data.loc[(df_data.index>start)&(df_data.index<timeStamp)][[ticker_X,ticker_Y]]
+    return df_data[ticker_Y].values - beta * df_data[ticker_X].values
+
+
+def get_half_life(Z):
+    
+    z_array = get_z(stockX,stockY,beta,df_is)
+
+    z_lag = np.roll(Z,1)
+    z_lag[0] = 0
+    z_ret = Z - z_lag
+
+    # adds intercept terms to X for regression
+    z_lag2 = add_constant(z_lag)
+    model = OLS(z_ret,z_lag2)
+    res = model.fit()
+
+    return int(-log(2)/res.params[1])
