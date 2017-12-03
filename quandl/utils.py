268c1f842c35935a2c4a4d5f50eee3647da712fa
alex <alex>
2017-12-03 15:24:16 +0100

    edit file

--- quandl/utils.py
+++ quandl/utils.py
@@ -1,5 +1,6 @@
 import dataiku as dk
 import numpy as np
+import datetime as dt
 
 def get_tickerDic(dfNm="sp500_ticker"):
     df_sym= dk.Dataset(dfNm).get_dataframe()
@@ -14,4 +15,5 @@ def get_price_dataframe(dfNm="wiki_sp500_daily_2007"):
 
 def get_Z(ticker_X,ticker_Y,beta,const,timeStamp,df_data):
     df_data = df_data.loc[df_data.index<timeStamp][[ticker_X,ticker_Y]]
+    dlta = dt.timedelta(days=252*7.5)
     return 
