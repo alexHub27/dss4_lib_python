50a8d48f92704215dcd8c66c20bf8be2a5d416a1
alex <alex>
2017-12-03 11:56:25 +0100

    edit file

--- quandl/utils.py
+++ quandl/utils.py
@@ -1,7 +1,7 @@
 import dataiku as dk
 import numpy as np
 
-def get_tickerDic(dfNm=sp500_ticker):
+def get_tickerDic(dfNm="sp500_ticker"):
     df_sym= dk.Dataset(dfNm).get_dataframe()
     sectors = np.unique(df_sym["GICS Sector"].values)
     return {tck:df_sym["GICS Sector"].values[idx] for idx,tck in enumerate(df_sym["Ticker symbol"].values)} 
