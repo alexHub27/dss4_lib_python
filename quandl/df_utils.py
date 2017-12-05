0bee8327657b0cfbd6f142b091ea2a60ff8194e8
alex <alex>
2017-12-05 16:28:30 +0100

    edit file

---
+++ quandl/df_utils.py
@@ -0,0 +1,67 @@
+import pandas as pd
+import numpy as np
+
+from statsmodels.tsa.stattools import adfuller,coint,add_constant
+from statsmodels.api import OLS
+
+import datetime as dt
+
+def get_corrLst(df_is,tickerDic):
+    #  called in main
+    corrList = []
+    for idx,tickX in enumerate(df_is.columns):
+        for j in range(idx+1,len(df_is.columns)):
+            tickY = df_is.columns[j]
+            if tickerDic[tickX] == tickerDic[tickY]:
+                corr = np.corrcoef(df_is[tickX].values,df_is[tickY].values)[0,1]
+                dist = np.sum(np.power(df_is[tickX].values-df_is[tickY].values,2))
+                if corr >0.9:
+                    corrList.append([tickX,tickY,corr,dist])
+
+    print "There are {0} pairs strongly correlated.".format(len(corrList))
+    return corrList
+
+def get_cointLst(corrList,df_is):
+    # called in main
+    # Test cointegration the test has to be perform on both side of the spread
+    cointLst = []
+    for pair in corrList:
+        X1,X2 = df_is[pair[0]].values,df_is[pair[1]].values 
+
+        x1 = add_constant(X1) 
+        x2 =add_constant(X2)
+        r1 = OLS(X2,x1).fit() 
+        r2 = OLS(X1,x2).fit()
+
+        adf1= adfuller(r1.resid)[1] 
+        if adf1<0.01 : 
+            adf2 = adfuller(r2.resid)[1]
+            if adf2<0.01 and adf1 < adf2: # Test for strong cointegration in both side only.
+                cointLst.append(["{0}_{1}".format(pair[0],pair[1])]+pair+[adf1]+list(r1.params))
+            else:
+                cointLst.append(["{0}_{1}".format(pair[1],pair[0])]+[pair[1],pair[0],pair[2],pair[3],adf2]+list(r2.params))
+                            
+    print "There are {0} pairs strongly cointegrated.".format(len(cointLst))
+    return cointLst
+
+def get_df_coint(cointLst,tickerDic,df_is):
+    # called in main
+    # Make df_coint
+    df_coint = pd.DataFrame(np.array(cointLst),columns=["spreadNm","stock_X","stock_Y","coefcorr","dist"
+                                                        ,"adf","const","beta"])
+    # Computing half life
+    df_coint["half_life"] = [get_half_life(tck,df_coint.stock_X.values[idx],df_coint.beta.values[idx],df_is) 
+                             for idx,tck in enumerate(df_coint.stock_X.values)]
+    df_coint["sector"] = [tickerDic[tck] for tck in df_coint.stock_X.values]
+    
+    
+    # Compute the mean and the std per pairs
+    df_coint["stdv"] = [std_z(X,df_coint.stock_Y.values[idx],df_coint.beta.values[idx],df_is) 
+                       for idx,X in enumerate(df_coint.stock_X.values)]
+    df_coint["lastPrice"] = [get_z(X,df_coint.stock_Y.values[idx],df_coint.beta.values[idx],df_is)[-1] 
+                             for idx,X in enumerate(df_coint.stock_X.values)]
+    df_coint["last_Zscore"] = [(lp - np.float(df_coint.const.values[idx]))/np.float(df_coint.stdv.values[idx])
+                               for idx,lp in enumerate(df_coint.lastPrice.values)]
+    
+    print "Df_shape : ",df_coint.shape
+    return df_coint
