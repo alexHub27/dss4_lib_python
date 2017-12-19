bcfb32a513bdcfbd4ba0ffe08a763aba97245fd9
alex <alex>
2017-12-19 12:41:18 +0100

    edit file

--- quandlb/api_call.py
+++ quandlb/api_call.py
@@ -3,7 +3,9 @@ import quandl as qd
 def get_data_from_quand(tick,start_date="2004-12-31",end_date="2017-06-30"):
     qd.ApiConfig.api_key = 'w-DrTrcoKxr4oxczgsTB'
     mydata =  qd.get(['WIKI/{0}'.format(tick)],start_date=start_date,end_date=end_date)
+    print "Getting data from quandl for {0} from {1} to {2} - {3} lines retrieved from {4} to {5}".format()
     mydata = mydata.reset_index()
+
     return mydata
 
 def get_dic_from_quandl(mydata):
