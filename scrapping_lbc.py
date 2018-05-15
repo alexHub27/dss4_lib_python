99be7eb92f3ac147de20806b4968834f11efd96d
alexHub27 <alexandre.hubert@dataiku.com>
2018-05-15 14:32:31 +0100

    Create scrapping_lbc.py

---
+++ scrapping_lbc.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import requests as rq
+import pandas as pd
+import random as rd
+import datetime as dte
+import time 
+import re
+from multiprocessing import Pool
+from bs4 import BeautifulSoup as Soup
